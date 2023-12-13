d35d5ffd8d227c233f6ea3d556eab125b466bd12
Harshil <harshilsitapara4@gmail.com>
2023-12-13 12:56:44 +0530

    changed package manager

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app=express()
+const PORT = Number(process.env.PORT) || 8000
+
+app.get("/",(req,res)=>{
+    res.json({message:"Port is running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`)
+})
